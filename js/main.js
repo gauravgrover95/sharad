3bcbd632323a94fdf31e1c38ed07101a352ded2b
Gaurav Grover <gauravgrover95@gmail.com>
2016-03-15 18:23:49 +0530

    logo feature added

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@
 $(".something").click(function() {
 	$(".about").toggle().addClass("animated bounceInUp");
 });
+
+$(".logo").mouseenter(function(){
+    $(".logo h1").fadeOut();
+    $(".logo p").fadeIn();
+});
+$(".logo").mouseleave(function(){
+    $(".logo h1").fadeIn();
+    $(".logo p").fadeOut();
+});
