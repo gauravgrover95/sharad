497416397859e82af3d6c6f617f1aba99f066ae4
Gaurav Grover <gauravgrover95@gmail.com>
2016-03-15 19:30:11 +0530

    Hide Animation slowed down | Bug still exist | Testing sm for mobile view

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 
 
 $(".iclose").click(function() {
-	$(".about").addClass("animated fadeOutDownBig");
-
+	// $(".about").addClass("animated fadeOutDownBig");
+	$(".about").css("animation-duration", "0.8s").removeClass("animated bounceInUp").addClass("animated slideOutDown");
+    // setTimeout(function(){ 
+    // 	alert("Hello"); 
+    // 	$(".about").hide();
+    // }, 1000);
 });
 
 $(".link").click(function() {
-	$(".about").show().addClass("animated bounceInUp");
+	$(".about").css("animation-duration", "1.5s").show().removeClass("animated bounceInUp").addClass("animated bounceInUp");
 });
 
 $(".logo").mouseenter(function(){
